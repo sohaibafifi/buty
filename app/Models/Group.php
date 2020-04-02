9413c0a7b61191c099ca5abbc0676cd31754334d
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-04-02 16:23:29 +0200

    Remove unused files

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
     protected $fillable = [
-        'name', 'scodocId','cal'
+        'name', 'scodocId', 'cal'
     ];
     public function semestre()
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use SemestreUser;
 use Illuminate\Notifications\Notifiable;
