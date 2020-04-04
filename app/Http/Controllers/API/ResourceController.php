05582d407218a08f98e88f0bceeedf4ec361e07c
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-04-04 11:34:36 +0200

    Add swagger doc

--- app/Http/Controllers/API/ResourceController.php
+++ app/Http/Controllers/API/ResourceController.php
@@ -6,6 +6,13 @@ use Illuminate\Support\Str;
 use App\Http\Requests\ApiRequest;
 use App\Http\Controllers\Controller;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Buty resource api",
+ *      description="Buty Swagger OpenApi description",
+ * )
+ */
 class ResourceController extends Controller
 {
     /**
@@ -14,6 +21,44 @@ class ResourceController extends Controller
      * @param  string  $resource
      * @param  int  $id
      * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * @OA\Get(
+     *      path="/api/{resource}/{id}",
+     *      operationId="show",
+     *      tags={"Resource"},
+     *      summary="Get Resource information",
+     *      description="Returns resource",
+     *      @OA\Parameter(
+     *          name="resource",
+     *          description="Resource name",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Resource id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       ),
+     *       @OA\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_key_security_example": {}}
+     *       }
+     *     )
+     *
+     * Get Resource information
      */
     public function show(ApiRequest $request, $resource, $id)
     {
