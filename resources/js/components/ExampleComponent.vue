edb6093b113f6dde8bac276bf0a64710f15c54cf
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-04-06 15:53:54 +0200

    replace bootstrap with tailwindcss

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+  <div class="flex items-center">
+    <div class="md:w-1/2 md:mx-auto">
+      <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+        <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">Example Component</div>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+        <div class="w-full p-6">
+          <p class="text-gray-700">I'm an example component.</p>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
 </script>
